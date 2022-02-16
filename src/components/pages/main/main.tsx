36201bb8aa76a0d9233c3f66f65f231bdc75b229
eetu <you@example.com>
2022-02-16 14:55:12 +0200

    kuva ja tekstia

--- src/components/pages/main/main.tsx
+++ src/components/pages/main/main.tsx
@@ -8,9 +8,14 @@ type MainProps = {
 const Main: React.FC<MainProps> = () => {
   return (
     <div className={styles.container}>
+      <img src='https://i.pinimg.com/originals/31/88/35/318835b0a606b4d2d4cdaa3154ed5aa9.jpg' alt="stickman" className="container"></img>
+
       <h1><b>Juuso Huuskonen</b></h1>
 
-      <p>Hi! This is my portfolio. Here you will find my cv and few of my projects. I study information and communication technology</p>
+      <p>
+         Hi! This is my portfolio. Here you will find my cv and few of my projects. 
+         I study information and communication technology at vocational college Varia: Myyrmäki office. 
+     </p>
 
         <hr></hr>
 
