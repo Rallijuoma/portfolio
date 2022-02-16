332dd62d5050b44679d1af6e52ef54f5c9fcec19
eetu <you@example.com>
2022-02-16 12:24:51 +0200

    projecteja lisätty

--- src/components/pages/projects/projects.tsx
+++ src/components/pages/projects/projects.tsx
@@ -8,7 +8,9 @@ type ProjectsProps = {
 const Projects: React.FC<ProjectsProps> = () => {
   return (
     <div className={styles.container}>
-      <a href='https://github.com/varia-ict/varia-agile-20C-ryhma-2'>Unity group project</a>
+      <a href='https://github.com/varia-ict/varia-agile-20C-ryhma-2'>Unity group project github page</a>
+      <a href='https://play.unity.com/mg/other/wall-hopper-mac-build'>Playable link to the unity group project</a>
+      <a href='https://github.com/Rallijuoma/portfolio'>Github page for portfolio</a>
     </div>
   )
 }
