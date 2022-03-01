fc12c0321a2234bcb5e60822be6f8a0915107d1d
Rallijuoma <huuskonen39@gmail.com>
2022-03-01 15:07:02 +0200

    a

--- src/components/pages/cv/cv.tsx
+++ src/components/pages/cv/cv.tsx
@@ -9,9 +9,13 @@ const Cv: React.FC<CvProps> = () => {
   return (
     <div className={styles.container}>
       
-      <h1>CV</h1>
+      <h1>EDUCATION</h1>
 
-     {/* <img src='https://binaries.templates.cdn.office.net/support/templates/fi-fi/lt16402487_quantized.png' alt='cv' className="container"/> */}
+        <h3>Vocational qualification</h3>
+        <h6>Vocational college Varia: Myyrmäki office</h6>
+        <p>6/08/2020  {'  >'}</p>
+          
+       
 
     </div>
   )
