f0453500882919466b88a30f3559323ed8e4c057
Rallijuoma <huuskonen39@gmail.com>
2022-03-08 14:00:55 +0200

    projects

--- src/components/pages/projects/projects.tsx
+++ src/components/pages/projects/projects.tsx
@@ -10,7 +10,10 @@ const Projects: React.FC<ProjectsProps> = () => {
     <div className={styles.container}>
           <a href='https://github.com/varia-ict/varia-agile-20C-ryhma-2' target="_blank" rel="noreferrer" > Unity group project github page</a>
           <a href='https://play.unity.com/mg/other/wall-hopper-mac-build' target="_blank" rel="noreferrer" >Playable link to the unity group project</a>
+          <p>This is a 2D platform game. You have to get trough 5 different levels with each having a different theme.
+          </p>
           <a href='https://play.unity.com/u/JuusoHuuskonen'  target="_blank" rel="noreferrer" >My unity play profile</a>
+          <p>This is my unity play profile here you will find my unity games and exercises. </p>
       </div>
       
   )
